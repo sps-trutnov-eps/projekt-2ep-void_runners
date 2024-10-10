4dbe2b1ca3747dd63a6722a42b4d99a7ca9b630b
alter-at-sps <soucekd23@spstrutnov.cz>
2024-10-10 13:31:54 +0200

    add asset dir var to editor

--- source/editor.py
+++ source/editor.py
@@ -1,6 +1,6 @@
 # == pre-imports ==
 
-import OpenGL
+import OpenGL, os
 OpenGL.ERROR_CHECKING = True
 OpenGL.ERROR_LOGGING = True
 
@@ -13,4 +13,5 @@ except ImportError:
     print("[\x1b[1m\x1b[31merror\x1b[0m] There was a problem importing the On-Cue Editor, make sure you pip installed the requirements.txt *and* requirements_editor.txt and did a `git submodule update --init`")
     raise
 
+on_cue.EDITOR_ASSET_DIR = os.path.join(os.path.dirname(__file__), "../assets")
 on_cue.start_editor()
