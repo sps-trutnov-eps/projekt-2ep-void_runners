c09aaa6ed46c0b4a55cf97e9117501d9540f081e
Šimon Volk <Volks@spstrutnov.cz>
2024-10-10 17:53:01 +0200

    UI part 2 (už ho nemám rád)

--- source/ui.py
+++ source/ui.py
@@ -6,21 +6,31 @@ class GameUI:
         self.ammo = ammo
         self.score = score
 
-    def set_lives(self, lives):
-        self.lives = lives
-    
-    def set_ammo(self, ammo):
-        self.ammo = ammo
-
-    def set_score(self, score):
-        self.score = score
-    
     def render_ui(self):
-        imgui.begin("Game UI", False, imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_RESIZE)
+        window_width, window_height = 1280, 720  
+        
+        # Nastavení pozice okna na levý dolní roh
+        imgui.set_next_window_position(10, window_height - 150)  
+        imgui.set_next_window_size(300, 130)  
 
+       
+        imgui.begin("Game UI", False, imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE)
+       
         imgui.text(f"Lives: {self.lives}")
+        imgui.same_line(120)
+        imgui.text("[icon]")
+
         imgui.text(f"Ammo: {self.ammo}")
+        imgui.same_line(120)
+        imgui.text("[icon]")
+
         imgui.text(f"Score: {self.score}")
+        imgui.same_line(120)
+        imgui.text("[icon]")
 
         imgui.end()
-        
+
+
+
+
+
